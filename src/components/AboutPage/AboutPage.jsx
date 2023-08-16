097a41697bd7ab39aa62d182b1323bc42f300b20
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-08-16 16:34:18 +0600

    About page design done

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -6,27 +6,30 @@ const AboutPageCom = () => {
   return (
     <div>
       {/* image with relative text */}
-      <div>
+      <div className="w-full h-[300px] relative">
         <Image
           src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt="AboutPage Img"
-          className="w-full"
-          width={500}
-          height={100}
+          className="object-cover grayscale"
+          fill={true}
+          //   width=""
+          //   height=""
         />
         {/* relative text in image */}
-        <div>
-          <h2>Digital Storytellers</h2>
-          <h4>Handcrafting award winning digital experiences</h4>
+        <div className="absolute bottom-5 left-5 bg-[#53c28b] p-2 text-white">
+          <h2 className="text-4xl font-semibold">Digital Storytellers</h2>
+          <h4 className="text-2xl">
+            Handcrafting award winning digital experiences
+          </h4>
         </div>
       </div>
 
       {/* some content */}
-      <div>
+      <div className="md:flex gap-24 mt-12">
         {/* item 1 */}
-        <div>
-          <h2>Who Are We?</h2>
-          <p>
+        <div className="flex-1 flex flex-col gap-6">
+          <h2 className="text-3xl font-semibold">Who Are We?</h2>
+          <p className="text-lg font-light text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
             quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
             suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
@@ -43,9 +46,9 @@ const AboutPageCom = () => {
         </div>
 
         {/* item 2 */}
-        <div>
-            <h2>What We Do?</h2>
-            <p className="">
+        <div className="flex-1 flex flex-col gap-6">
+          <h2 className="text-3xl font-semibold">What We Do?</h2>
+          <p className="text-lg font-light text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
             quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
             suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
