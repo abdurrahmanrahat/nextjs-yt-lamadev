2a08c6a6ad93026e552f156d0996eec7d544f321
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-08-23 08:52:28 +0600

    fetch data in blog page

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,13 +1,27 @@
 import Image from "next/image";
-import React from "react";
+import { notFound } from "next/navigation";
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({ params }) => {
+  const singleData = await getData(params.id);
+
   return (
     <div className="">
       <div className="flex">
         <div className="flex-1 flex flex-col justify-between">
-          <h1 className="text-4xl">Title</h1>
-          <p className="text-lg font-light">Description</p>
+          <h1 className="text-4xl">{singleData.title}</h1>
+          <p className="text-lg font-light">{singleData.body}</p>
           <div className="flex items-center gap-3">
             <Image
               src="https://images.pexels.com/photos/7853221/pexels-photo-7853221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -29,7 +43,7 @@ const BlogPost = () => {
         </div>
       </div>
       <div className="mt-12 text-xl font-light text-[#999] text-justify">
-        <p className="">Lorem content</p>
+        <p className="">{singleData.body}</p>
       </div>
     </div>
   );
--- src/components/BlogPage/BlogPageCom.jsx
+++ src/components/BlogPage/BlogPageCom.jsx
@@ -2,65 +2,43 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const BlogPageCom = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const BlogPageCom = async () => {
+  const data = await getData();
+
   return (
     <div>
-      {/* blog 01 */}
-      <Link
-        href="/blog/testId"
-        className="flex items-center gap-10 mb-10"
-      >
-        <div className="w-1/3 h-[380px] relative ">
-          <Image
-            src="https://images.pexels.com/photos/7853221/pexels-photo-7853221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            fill={true}
-            className="object-cover"
-            alt="BlogImg"
-          />
-        </div>
-        <div className="w-2/3">
-          <h2 className="mb-2 text-4xl font-semibold">Test</h2>
-          <p className="text-lg text-[#999]">desc</p>
-        </div>
-      </Link>
-
-      {/* blog 02 */}
-      <Link
-        href="/blog/testId"
-        className="flex items-center gap-10 mb-10"
-      >
-        <div className="w-1/3 h-[380px] relative ">
-          <Image
-            src="https://images.pexels.com/photos/7853221/pexels-photo-7853221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            fill={true}
-            className="object-cover"
-            alt="BlogImg"
-          />
-        </div>
-        <div className="w-2/3">
-          <h2 className="mb-2 text-4xl font-semibold">Test</h2>
-          <p className="text-lg text-[#999]">desc</p>
-        </div>
-      </Link>
-
-      {/* blog 03 */}
-      <Link
-        href="/blog/testId"
-        className="flex items-center gap-10 mb-10"
-      >
-        <div className="w-1/3 h-[380px] relative ">
-          <Image
-            src="https://images.pexels.com/photos/7853221/pexels-photo-7853221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            fill={true}
-            className="object-cover"
-            alt="BlogImg"
-          />
-        </div>
-        <div className="w-2/3">
-          <h2 className="mb-2 text-4xl font-semibold">Test</h2>
-          <p className="text-lg text-[#999]">desc</p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link
+          key={item.id}
+          href="/blog/testId"
+          className="flex items-center gap-10 mb-10"
+        >
+          <div className="w-1/3 h-[380px] relative ">
+            <Image
+              src="https://images.pexels.com/photos/7853221/pexels-photo-7853221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              fill={true}
+              className="object-cover"
+              alt="BlogImg"
+            />
+          </div>
+          <div className="w-2/3">
+            <h2 className="mb-2 text-4xl font-semibold">{item.title}</h2>
+            <p className="text-lg text-[#999]">desc</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
