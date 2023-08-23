277bb120498aae3e2fa40b74c0195f0e4ac25eed
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-08-23 08:59:31 +0600

    added swr to fetch data

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,17 @@
-import React from 'react';
+"use client"
+
+import useSWR from 'swr'
 
 const DashboardPage = () => {
-    return (
-        <div>
-            DashboardPage
-        </div>
-    );
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  console.log(data);
+
+  return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
